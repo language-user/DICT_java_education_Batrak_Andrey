eb2082c8ea7c8d9035f624d6865ad007c01823ce
Batrak_Andrey <a.y.batrak@student.khai.edu>
2023-11-15 15:29:07 +0200

    hangman step3

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,6 +1,21 @@
+import java.util.*;
 
-public class hangman {
+public class Hangman {
     public static void main(String[] args) {
-        System.out.println("HANGMAN\nThe game will be available soon");
+        String[] words_arr = {"java", "kotlin", "python", "javascript"};
+        Random randomIntGen = new Random();
+        int randomInt = randomIntGen.nextInt(words_arr.length);
+
+        String word = words_arr[randomInt];
+        System.out.println("HANGMAN");
+        System.out.println("Guess the word:");
+        Scanner user_input = new Scanner(System.in);
+        String answer = user_input.nextLine();
+        if (answer.equals(word)){
+            System.out.println("You survived!");
+        }
+        else {
+            System.out.println("You lost!");
+        }
     }
 }
