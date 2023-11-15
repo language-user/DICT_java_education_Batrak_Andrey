6a8d8392f167015bc02afb5ea480a3475a70a591
Batrak_Andrey <a.y.batrak@student.khai.edu>
2023-11-15 15:30:41 +0200

    hangman step4

--- src/Hangman.java
+++ src/Hangman.java
@@ -5,12 +5,15 @@ public class Hangman {
         String[] words_arr = {"java", "kotlin", "python", "javascript"};
         Random randomIntGen = new Random();
         int randomInt = randomIntGen.nextInt(words_arr.length);
-
         String word = words_arr[randomInt];
+        StringBuilder hiddenWord = new StringBuilder(word.substring(0, 2));
+        for (int i = 0; i < word.length() - 2; i++){
+            hiddenWord.append('-');
+        }
         System.out.println("HANGMAN");
-        System.out.println("Guess the word:");
-        Scanner user_input = new Scanner(System.in);
-        String answer = user_input.nextLine();
+        System.out.printf("Guess the word %s:", hiddenWord);
+        Scanner userInput = new Scanner(System.in);
+        String answer = userInput.nextLine();
         if (answer.equals(word)){
             System.out.println("You survived!");
         }
