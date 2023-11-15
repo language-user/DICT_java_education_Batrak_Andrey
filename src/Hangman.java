3ad8d15d38b2dbce67310234b3ca1d02f6fbfa16
Batrak_Andrey <a.y.batrak@student.khai.edu>
2023-11-15 15:34:48 +0200

    hangman step7

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,76 +1,96 @@
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class Hangman {
-    private static int lives=8;
-
+    String[] words_arr = {"java", "kotlin", "python", "javascript"};
+    String word = "";
+    StringBuilder usedChars = new StringBuilder();
+    StringBuilder secretWord = new StringBuilder();
+    Scanner input = new Scanner(System.in);
+    int lives = 8;
     public static void main(String[] args) {
-        String[] words_arr = {"java", "kotlin", "python", "javascript"};
-        String word = generateWord(words_arr);
-        StringBuilder hiddenWord = hiddenWordGen(word);
-        StringBuilder chosenLetters = new StringBuilder();
-        System.out.println("HANGMAN");
-
+        Hangman myGame = new Hangman();
+        myGame.startGame();
+    }
+    public void startGame(){
         while (true) {
-            if (lives == 0) {
-                System.out.println("You lost!");
-                break;
-            } else {
-                if (hiddenWord.indexOf("-") != -1) {
-                    System.out.printf("Guess the word %s:", hiddenWord);
-                    Scanner userInput = new Scanner(System.in);
-                    String answer = userInput.nextLine();
-                    if (checker(answer, chosenLetters)) {
-                        chosenLetters.append(answer);
-                        if (!word.contains(answer)) {
-                            System.out.println("The letter doesn't appear in word");
-                            --lives;
+            System.out.println("HANGMAN");
+            System.out.println("Type \"play\" to play the game or \"exit\"");
+            String choice = input.nextLine();
+            if (Objects.equals(choice, "play")){
+                setRandomWord();
+                createSecretWord();
+                while (true) {
+                    if (lives == 0) {
+                        System.out.println("You lost!");
+                        break;
+                    } else {
+                        if (secretWord.indexOf("-") != -1) {
+                            makeMove();
                         } else {
-                            updateHiddenWord(hiddenWord, answer, word);
+                            System.out.println(secretWord);
+                            System.out.println("You win!");
+                            break;
                         }
                     }
-                } else {
-                    System.out.println(hiddenWord);
-                    System.out.println("You win!");
-                    break;
                 }
+                lives = 8;
+                word = "";
+                usedChars = new StringBuilder();
+                secretWord = new StringBuilder();
+
+            } else if (Objects.equals(choice, "exit")) {
+                break;
+            }
+        }
+    }
+    public void makeMove() {
+        System.out.printf("Guess the word %s:", secretWord);
+        String choice = input.nextLine();
+        if (checker(choice)) {
+            usedChars.append(choice);
+            if (!word.contains(choice)) {
+                System.out.println("The letter doesn't appear in word");
+                --lives;
+            } else {
+                updateSecretWord(choice);
             }
         }
     }
 
-    public static String generateWord(String[] words_arr) {
-        Random randomIntGen = new Random();
-        int randomInt = randomIntGen.nextInt(words_arr.length);
-        return words_arr[randomInt];
+    public void setRandomWord() {
+        Random myRandom = new Random();
+        int randomInt = myRandom.nextInt(words_arr.length);
+        word = words_arr[randomInt];
     }
 
-    public static StringBuilder hiddenWordGen(String word) {
-        StringBuilder hiddenWord = new StringBuilder();
-        hiddenWord.append("-".repeat(word.length()));
-        return hiddenWord;
+    public void createSecretWord() {
+        secretWord.append("-".repeat(word.length()));
     }
 
-    public static void updateHiddenWord(StringBuilder hiddenWord, String answer, String word) {
+
+    public void updateSecretWord(String choice) {
         int index = 0;
         while (index >= 0) {
-            index = word.indexOf(answer, index);
+            index = word.indexOf(choice, index);
             if (index >= 0) {
-                hiddenWord.setCharAt(index, answer.charAt(0));
+                secretWord.setCharAt(index, choice.charAt(0));
                 ++index;
             }
         }
     }
-    public static Boolean checker(String answer, StringBuilder chosen_letters) {
-        String letters = "qwertyuiopasdfghjklzxcvbnm";
-        if (answer.length() != 1) {
-            System.out.println("You should enter one letter");
+    public Boolean checker (String choice) {
+        Pattern p = Pattern.compile("[A-Za-z]");
+        if (choice.length() != 1) {
+            System.out.println("You should input a single letter");
             return false;
         }
-        else if (!letters.contains(answer)){
-            System.out.println("You should enter only english lowercase letters!");
+        else if (!p.matcher(choice).matches()){
+            System.out.println("You should enter only letters!");
             return false;
         }
-        else if (chosen_letters.indexOf(answer) != -1){
-            System.out.println("No improvements");
+        else if (usedChars.indexOf(choice) != -1){
+            System.out.println("You've already guessed this letter");
             --lives;
             return false;
         }
