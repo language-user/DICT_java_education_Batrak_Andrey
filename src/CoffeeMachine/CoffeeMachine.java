0ada8463ef63bf8a27ed62e7f4d2cbba4e0475e9
Batrak_Andrey <a.y.batrak@student.khai.edu>
2023-11-21 15:48:29 +0200

    coffeemachine stage 1

---
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -0,0 +1,19 @@
+package CoffeeMachine;
+import java.util.Scanner;
+
+public class CoffeeMachine {
+
+    public static void main(String[] args) {
+        System.out.println("Starting to make a coffee\n" +
+                "Grinding coffee beans\n" +
+                "Boiling water\n" +
+                "Mixing boiled water with crushed coffee beans\n" +
+                "Pouring coffee into the cup\n" +
+                "Pouring some milk into the cup\n" +
+                "Coffee is ready!\n");
+
+
+
+    }
+}
+
