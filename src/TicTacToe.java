903ce2abfefc0d8bcb02e76a68b3d222c1bd2784
Batrak_Andrey <a.y.batrak@student.khai.edu>
2023-12-06 10:51:53 +0200

    TicTacToe stage 4

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,22 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+class OutBoundsMoveException extends Exception {}
+class OccupiedCoordinateException extends Exception {}
+
+
+class Coordinates {
+    public final int x;
+    public final int y;
+    Coordinates(int x, int y) throws OutBoundsMoveException {
+        if (x < 0 || x > 2 || y < 0 || y > 2) {
+            throw new OutBoundsMoveException();
+        }
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class TicTacToe {
     public static String[][] getRows(String[] gameState) {
@@ -147,7 +164,7 @@ public class TicTacToe {
         return line.strip().split("");
     }
 
-    public static void Board(String[] gameState) {
+    public static void printBoard(String[] gameState) {
         System.out.println("---------");
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
@@ -177,10 +194,44 @@ public class TicTacToe {
         return status;
     }
 
+    public static Coordinates readUserMove() throws OutBoundsMoveException {
+        Scanner scanner = new Scanner(System.in);
+        int y = scanner.nextInt() - 1;
+        int x = scanner.nextInt() - 1;
+        return new Coordinates(x, y);
+    }
+
+    public static void makeUserMove(Coordinates move, String[] gameState) throws OccupiedCoordinateException {
+        String target = gameState[move.y * 3 + move.x];
+
+        if (target.equals("X") || target.equals("O")) {
+            throw new OccupiedCoordinateException();
+        }
+
+        gameState[move.y * 3 + move.x] = "X";
+    }
+
+    public static void processUserMove(String[] gameState) {
+        try {
+            System.out.print("Enter the coordinates: ");
+            Coordinates move = readUserMove();
+            makeUserMove(move, gameState);
+        } catch (OutBoundsMoveException e) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            processUserMove(gameState);
+        } catch (OccupiedCoordinateException e) {
+            System.out.println("This cell is occupied! Choose another one!");
+            processUserMove(gameState);
+        } catch (InputMismatchException e) {
+            System.out.println("You should enter numbers!");
+            processUserMove(gameState);
+        }
+    }
+
     public static void main(String[] args) {
         String[] gameState = readGameState();
-        Board(gameState);
-        String status = getStatus(gameState);
-        System.out.println(status);
+        printBoard(gameState);
+        processUserMove(gameState);
+        printBoard(gameState);
     }
 }
