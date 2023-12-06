0db59aec63e15a06ca42835c81db7ee7c7a8fe30
Batrak_Andrey <a.y.batrak@student.khai.edu>
2023-12-06 10:28:10 +0200

    stage 1

---
+++ src/TicTacToe.java
@@ -0,0 +1,8 @@
+public class TicTacToe {
+    public static void main(String[] args){
+        System.out.println("""
+                X O X
+                O X O
+                X X O""");
+    }
+}
